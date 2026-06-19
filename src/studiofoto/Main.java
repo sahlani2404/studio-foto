e6c53db02e4aa29279adaf769ae50482811ea6b2
Sahlani <sahlani@gmail.com>
2026-06-19 23:32:20 +0700

    Project Studio Foto NetBeans

---
+++ src/studiofoto/Main.java
@@ -0,0 +1,15 @@
+package studiofoto;
+
+public class Main {
+    
+    public static void main(String[] args)
+{
+
+java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FormLogin().setVisible(true);
+            }
+        });
+
+    }
+}
